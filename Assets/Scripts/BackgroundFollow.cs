447af1c1530bb0e55c24988a8e2b696ed6150338
behowtour <37346495+behowtour@users.noreply.github.com>
2022-05-25 19:57:34 +0300

    Автопозиционирование background
    +Проверил и доработал чтобы нормально работало автопозиционирование фона.
    +Поправил Score, чтобы привязано к углу было и изменил размер шрифта

--- Assets/Scripts/BackgroundFollow.cs
+++ Assets/Scripts/BackgroundFollow.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class BackgroundFollow : MonoBehaviour
 {
-    
-    Camera cam;
     private float backgroundOffsetToCam ;
-
+    public Camera cam;
     public Transform target;
-    // Start is called before the first frame update
+    
+
     void Start()
     {
-        //Vector3 botLeftCamCoordinate = cam.ScreenToWorldPoint(0,0,0);
-        //float yCamBotCoordinate = botLeftCamCoordinate.y; //нижняя часть камеры
-        //float backgroundHalfY = transform.position.y + transform.localScale.y/2;
-        backgroundOffsetToCam = transform.localScale.y/2 - cam.localScale.y/2; //нижняя часть бэкграунда
-        transform.position.y = cam.transform.position.y + backgroundOffsetToCam;
+        var backgroundHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+        backgroundOffsetToCam = backgroundHeight / 2 - cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0) ).y; //нижняя часть бэкграунда
+        
+        Vector3 newPos = new Vector3(transform.position.x, target.transform.position.y + backgroundOffsetToCam, transform.position.z);
+        transform.position = newPos;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        Vector3 newPos = new Vector3(transform.position.x, target.position.y+3.38f-target.position.y*0.4f+backgroundOffsetToCam, transform.position.z);
+        Vector3 newPos = new Vector3(transform.position.x, target.position.y-target.position.y*0.1f+backgroundOffsetToCam, transform.position.z);
+        
         transform.position = newPos;
     }
 }
