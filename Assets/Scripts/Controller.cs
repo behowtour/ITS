f5d03d56ce6e6a1817d90295e01fa9e809d957fa
behowtour <37346495+behowtour@users.noreply.github.com>
2022-06-01 21:11:29 +0300

    заговнокодил ограничение скорости падения в контроллере

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,12 +8,13 @@ public class Controller : MonoBehaviour
     public bool isMouseHoldOnAnchor;
     public float force;
     public float sparrowRatio;
+    public float maxDownSpeedY;
 
     [Header("Dynamic variables")]
     public GameObject hittedAnchor, lastHittedAnchor;
     public Vector2 ropeLengthVec;
     public float power;
-    
+    public float xSpeed, ySpeed, velMagnSpeed;
 
     private new Rigidbody2D rigidbody;
     
@@ -22,7 +23,19 @@ public class Controller : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
-    
+
+    private void Update()
+    {
+        xSpeed = rigidbody.velocity.x;
+        ySpeed = rigidbody.velocity.y;
+        velMagnSpeed = rigidbody.velocity.magnitude;
+        if (rigidbody.velocity.y <= (-1)*maxDownSpeedY)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, (-1) * maxDownSpeedY);
+        }
+        
+    }
+
     public void HitPoint()
     {
         if (Input.GetMouseButton(0) && isMouseHoldOnAnchor)
