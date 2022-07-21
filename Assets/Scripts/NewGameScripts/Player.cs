71b673379ab2854309dbd3a3a418ceadc91540e1
behowtour <37346495+behowtour@users.noreply.github.com>
2022-07-21 21:39:45 +0300

    не доделано

--- Assets/Scripts/NewGameScripts/Player.cs
+++ Assets/Scripts/NewGameScripts/Player.cs
@@ -19,8 +19,8 @@ public class Player : MonoBehaviour
     {
         lastOrdValue = this.transform.position.y;
         lastVelocity = 0;
-        InitPlayerStates();
-        SetPlayerStateByDefault();
+        //InitPlayerStates();
+        //SetPlayerStateByDefault();
     }
 
     // Update is called once per frame
