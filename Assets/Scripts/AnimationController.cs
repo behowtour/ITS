de8f67bde82cc26bfccfe786fee80743e3d4ddaa
Sparrowack <37346702+Sparrowack@users.noreply.github.com>
2022-07-27 20:44:40 +0300

    качание ГГ вернул

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,42 +7,24 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
 
-    public float angle;
+    
     public GameObject hero;
     public Vector2 vel;
-    public Vector2 velN;
     public Vector3 newRot;
     Rigidbody2D rigid;
-    public Vector3 glassesPos;
-    public GameObject glasses;
-    Animator animator;
+ 
     
     void Start()
     {
         rigid = hero.GetComponent<Rigidbody2D>();
-        glassesPos = glasses.transform.localPosition;
-        animator = GetComponent<Animator>();
     }
 
    
     void Update()
     {
-
-       
-
-
-
         vel = rigid.velocity.normalized;
-        velN = rigid.velocity;
         newRot = Vector3.forward;
         newRot.z = -vel.x * 30;
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(newRot), 0.8f *Time.deltaTime);
-        float offset = Mathf.Clamp(velN.y / 10, -1, 1);
-        glasses.transform.localPosition = new Vector2(glassesPos.x + offset * -0.3f, glassesPos.y);
-        animator.SetFloat("velocity", offset);
-      //  float clipLenght=  animator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
-       // animator.Play("AnimDyn", 0, (offset+1)/2);
-       
-        animator.speed = 0;
     }
 }
