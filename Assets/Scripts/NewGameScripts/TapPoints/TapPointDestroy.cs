61b3c71887668d7dbebe9e28ee8e4a71a62643f2
behowtour <bbbehowtour@gmail.com>
2022-08-11 16:06:04 +0300

    gameOver по столкновению
    добавлен слой плеер
    добавлено событие в TapPointDestroy  - OnTriggerEnter2D
    подвинул коллайдер на герое
    проставлено "isTrigger" в префабах поинтов

--- Assets/Scripts/NewGameScripts/TapPoints/TapPointDestroy.cs
+++ Assets/Scripts/NewGameScripts/TapPoints/TapPointDestroy.cs
@@ -7,7 +7,7 @@ public class TapPointDestroy : MonoBehaviour, ITapPoint
 
     public void OnCollision()
     {
-        
+        GameOver.CheckGameOver(this.transform.gameObject.tag);
     }
 
     public void OnRelease()
@@ -28,5 +28,22 @@ public class TapPointDestroy : MonoBehaviour, ITapPoint
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+        if (collision.gameObject.tag == "Player")
+        {
+            OnCollision();
+        }
+        //OnCollision();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            OnCollision();
+        }
     }
 }
--- Assets/Scripts/NewGameScripts/TapPoints/TapPointFade.cs
+++ Assets/Scripts/NewGameScripts/TapPoints/TapPointFade.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TapPointFade : MonoBehaviour
+public class TapPointFade : MonoBehaviour, ITapPoint
 {
+    public void OnCollision()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnRelease()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTap()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
