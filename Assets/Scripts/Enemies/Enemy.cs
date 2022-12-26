73bd0f90d45e530124ceef3d2f6221a5d559acee
behowtour <37346495+behowtour@users.noreply.github.com>
2022-12-26 23:03:11 +0300

    EnemyMidge патрулирование влево вправо

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/Enemies/EnemyMidge.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMidge : Enemy
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D myRigodbody;
+    BoxCollider2D myBoxCollider;
+
+    void Start()
+    {
+        myRigodbody = GetComponent<Rigidbody2D>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            myRigodbody.velocity = new Vector2(moveSpeed, 0f);
+        }else
+        {
+            myRigodbody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x < Mathf.Epsilon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionCollider)
+    {
+        if (collisionCollider.CompareTag("MainCamera"))
+        {
+            transform.localScale = new Vector2(transform.localScale.x * (-1), transform.localScale.y);
+        }
+    }
+}
